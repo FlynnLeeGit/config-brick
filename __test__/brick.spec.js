cbdfe292a02e3cb9739c3d9bf0fe0521e18c17d1
FlynnLee <593029372@qq.com>
2018-02-17 18:32:17 +0800

    half

--- __test__/brick.spec.js
+++ __test__/brick.spec.js
@@ -1,502 +1,190 @@
-const $b = require('../index')
-const fse = require('fs-extra')
-const chalk = require('chalk')
+const $ = require('../index')
 
-const fn1 = opts => conf => ((conf.a = 1), conf)
-const fn2 = opts => conf => ((conf.b = 2), conf)
-
-const clearBrick = (...bricks) => {
-  bricks.forEach(b => {
-    $b.prototype[b] = null
-    $b.bricks[b] = null
-  })
+const fn1 = conf => {
+  conf.a = 1
+}
+const fn2 = conf => {
+  conf.b = 2
+  return conf
+}
+const fn3 = conf => {
+  conf.c = 3
+  return {
+    c: 3
+  }
+}
+const fn4 = (conf, next) => {
+  conf.d = 4
+  next()
 }
 
-describe('ConfigBrick', () => {
-  test('ConfigBrick exist', () => {
-    expect($b).toBeDefined()
+const fn5 = (conf, next) => {
+  setTimeout(() => {
+    conf.e = 5
+    next(conf)
   })
-  test('default config is an empty object', () => {
-    expect($b().value()).toEqual({})
-  })
-  test('should use seed object', () => {
-    expect($b({ a: 1 }).value()).toEqual({
-      a: 1
+}
+const fn6 = (conf, next) => {
+  setTimeout(() => {
+    conf.f = 6
+    next({
+      f: 6
     })
   })
-
-  test('should output json in default location', () => {
-    const configPath = __dirname + '/../config.json'
-    fse.removeSync(configPath)
-    $b()
-      .toJson()
-      .done()
-    const config = fse.readJsonSync(configPath, 'utf-8')
-    expect(config).toEqual({})
-    fse.removeSync(configPath)
-  })
-
-  test('should output correct json file', () => {
-    const configPath = __dirname + '/dist/config.json'
-    fse.removeSync(configPath)
-    $b.use({
-      fn1
+}
+const fn7 = conf =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      conf.g = 7
+      resolve()
     })
-    $b()
-      .fn1()
-      .toJson(configPath)
-      .done()
-    const config = fse.readJsonSync(configPath, 'utf-8')
-    expect(config).toEqual({ a: 1 })
-    clearBrick('fn1')
-    fse.removeSync(configPath)
   })
-
-  test('should async output json ok', done => {
-    const configPath = __dirname + '/dist/async/config.json'
-    fse.removeSync(configPath)
-    const fn1 = () => (conf, next) => {
-      conf.a = 1
-      next()
-    }
-    $b.use({
-      fn1
+const fn8 = conf =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      conf.h = 8
+      resolve(conf)
     })
-    $b()
-      .fn1()
-      .toJson(configPath)
-      .done()
-      .then(() => {
-        const config = fse.readJsonSync(configPath, 'utf-8')
-        expect(config).toEqual({ a: 1 })
-        done()
-        clearBrick('fn1')
-        fse.removeSync(configPath)
+  }, 300)
+const fn9 = conf =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      conf.i = 9
+      resolve({
+        i: 9
       })
+    }, 200)
   })
 
-  test('should debug ok', () => {
-    const isDebug = $b().debug()._debug
-    expect(isDebug).toBeTruthy()
-  })
-  test('should throw when .registerBrick options get not an object', () => {
-    expect(() => {
-      $b.registerBrick('a')
-    }).toThrow()
-  })
-
-  test('should throw when brick builder is not function', () => {
-    expect(() => {
-      $b.registerBrick({
-        a: 'a'
-      })
-    }).toThrow()
-  })
-
-  test('should add bricks from object', () => {
-    $b.registerBrick({
-      fn1,
-      fn2
-    })
-    const res = $b()
-      .fn1()
-      .fn2()
-      .value()
-    expect(res).toEqual({
-      a: 1,
-      b: 2
-    })
-    // clear prototype
-    clearBrick('fn1', 'fn2')
-  })
-
-  describe('extend behaviors', () => {
-    const $nb = $b.extend()
-    test('should exist', () => {
-      expect($nb).toBeDefined()
-      expect($nb.Name).toBeDefined()
-      expect($nb.use).toBeDefined()
-    })
-    test('should registerBrick on just own Ctor', () => {
-      $b.registerBrick({
-        fn1,
-        fn2
-      })
-      $nb.registerBrick({
-        fn3: () => conf => ((conf.c = 3), conf)
-      })
-
-      expect(() => {
-        $b.fn3()
-      }).toThrow()
-
-      expect(
-        $nb()
-          .fn1()
-          .fn2()
-          .fn3()
-          .value()
-      ).toEqual({
-        a: 1,
-        b: 2,
-        c: 3
-      })
-    })
-  })
-  test('should merge config with seed', () => {
-    $b.registerBrick({
-      fn1,
-      fn2
-    })
-    const res = $b({ c: 3 })
-      .fn1()
-      .fn2()
-      .value()
-    expect(res).toEqual({
-      a: 1,
-      b: 2,
-      c: 3
-    })
-    clearBrick('fn1', 'fn2')
+describe('basic', () => {
+  test('should config-brick exist', () => {
+    expect($).toBeDefined()
   })
 })
 
-describe('pipe brick', () => {
+describe('lay brick', () => {
   test('should exist', () => {
-    expect($b.bricks.pipe).toBeDefined()
+    expect($.bricks.lay).toBeDefined()
+    expect($.bricks.layAsync).toBeDefined()
   })
-  test('should pipe conf data', () => {
-    expect(
-      $b()
-        .pipe([fn1(), fn2()])
-        .value()
-    ).toEqual({
+  test('should default right', () => {
+    const conf = $().lay()
+    expect(conf).toEqual({})
+  })
+  test('should seed right', () => {
+    const conf = $({ a: 1 }).lay()
+    expect(conf).toEqual({ a: 1 })
+  })
+  test('should sync bricks right', () => {
+    const conf = $().lay([
+      fn1, //
+      fn2
+    ])
+    expect(conf).toEqual({
       a: 1,
       b: 2
     })
   })
-  test('should ok when it is not a array', () => {
-    $b.use({
-      fn1
+  test('should async lay right', done => {
+    const p = $().layAsync([fn7])
+    p.then(conf => {
+      expect(conf).toEqual({ g: 7 })
+      done()
     })
-    const conf = $b()
-      .fn1()
-      .pipe()
-      .value()
-    expect(conf).toEqual({ a: 1 })
-    clearBrick('fn1')
+  })
+  test('should mix sync lay ok', () => {
+    const conf = $().lay([fn1, fn2, fn3])
+    expect(conf).toEqual({
+      c: 3
+    })
   })
 
-  test('should return conf when undefined data returned', () => {
-    expect(
-      $b()
-        .pipe([
-          conf => {
-            conf.d = 4
-          }
-        ])
-        .value()
-    ).toEqual({
-      d: 4
+  test('should mix sync && async brick ok', done => {
+    const p = $().layAsync([fn1, fn2, fn3, fn7, fn8, fn9])
+    p.then(conf => {
+      expect(conf).toEqual({
+        i: 9
+      })
+      done()
+    })
+  })
+  test('should throw when use lay with async brick', () => {
+    expect(() => {
+      $().lay([fn7])
+    }).toThrow()
+  })
+  test('should warn when use layAsync with all sync bricks', () => {
+    $().layAsync()
+  })
+})
+
+describe('merge brick', () => {
+  test('should merge right', () => {
+    const conf = $({
+      a: [1],
+      b: 2
+    }).merge({
+      a: [2],
+      b: 3
+    })
+    expect(conf).toEqual({
+      a: [1, 2],
+      b: 3
+    })
+  })
+  test('should not merge array', () => {
+    expect($({ a: [1] }).merge({ a: [1] })).toEqual({
+      a: [1]
     })
   })
 })
 
 describe('if brick', () => {
-  test('should exist', () => {
-    expect($b.bricks.if).toBeDefined()
-  })
-  test('should merge conf when if true', () => {
-    $b.use({
-      fn1
-    })
-    const conf = $b()
-      .if(true, conf =>
-        $b(conf)
-          .fn1()
-          .value()
-      )
-      .value()
-    expect(conf).toEqual({
-      a: 1
-    })
-    clearBrick('fn1')
-  })
-
-  test('should use default behavie', () => {
-    expect(
-      $b()
-        .if(true)
-        .value()
-    ).toEqual({})
-  })
-  test('should not merge conf when if false', () => {
-    const conf = $b()
-      .if(false, conf =>
-        $b(conf)
-          .fn1()
-          .value()
-      )
-      .value()
+  test('should if sync default ok', () => {
+    const conf = $().lay([$.bricks.if(true)])
     expect(conf).toEqual({})
   })
-
-  test('should work when array', () => {
-    expect(
-      $b()
-        .if(true, [fn1()])
-        .value()
-    ).toEqual({
-      a: 1
-    })
-    expect(
-      $b()
-        .if(false, [fn1()], [fn2()])
-        .value()
-    ).toEqual({
+  test('should if sync right', () => {
+    let bool = true
+    const conf = $().lay([
+      fn1, //
+      $.bricks.if(
+        bool, //
+        [fn2]
+      )
+    ])
+    expect(conf).toEqual({
+      a: 1,
       b: 2
     })
-  })
-
-  test('shoud correct when no return', () => {
-    expect(
-      $b()
-        .if(true, conf => {
-          conf.a = 3
-        })
-        .value()
-    ).toEqual({
-      a: 3
+    bool = false
+    const conf2 = $().lay([
+      fn1, //
+      $.bricks.if(
+        bool, //
+        [fn2],
+        [fn3]
+      )
+    ])
+    expect(conf2).toEqual({
+      c: 3
     })
-    expect(
-      $b()
-        .if(
-          false,
-          conf => {
-            conf.a = 3
-          },
-          conf => {
-            conf.b = 6
-          }
+  })
+  test('should async if ok', done => {
+    $()
+      .layAsync([
+        fn7, //
+        $.bricks.ifAsync(
+          true, //
+          [fn8]
         )
-        .value()
-    ).toEqual({
-      b: 6
-    })
-  })
-
-  test('should transform by basic type', () => {
-    expect(
-      $b()
-        .if(true, 1, 2)
-        .value()
-    ).toEqual(1)
-
-    expect(
-      $b()
-        .if(false, 1, 2)
-        .value()
-    ).toEqual(2)
-  })
-})
-
-describe('merge brick', () => {
-  test('exist', () => {
-    expect($b.bricks.merge).toBeDefined()
-  })
-  test('should merge when object key', () => {
-    const conf = $b({ a: 3 })
-      .merge({ a: 2 })
-      .value()
-    expect(conf).toEqual({
-      a: 2
-    })
-  })
-  test('should concat arr conf', () => {
-    const conf = $b({ a: [1] })
-      .merge({ a: [2] })
-      .value()
-    expect(conf).toEqual({ a: [1, 2] })
-  })
-  test('should not concat item when equal', () => {
-    expect(
-      $b({ a: [1] })
-        .merge({ a: [1] })
-        .value()
-    ).toEqual({ a: [1] })
-    expect(
-      $b({ a: [{ test: '3' }] })
-        .merge({ a: [{ test: '3' }] })
-        .value()
-    ).toEqual({
-      a: [{ test: '3' }]
-    })
-  })
-})
-
-describe('script brick ok', () => {
-  test('should exist', () => {
-    expect($b.prototype.script).toBeDefined()
-  })
-
-  test('should excute script', () => {
-    $b.use({
-      fn1
-    })
-    let count = 0
-    const conf = $b()
-      .script(conf => {
-        count++
-      })
-      .fn1()
-      .value()
-    expect(count).toBe(1)
-    expect(conf).toEqual({
-      a: 1
-    })
-    clearBrick('fn1')
-  })
-  test('should throw when script is not a function', () => {
-    expect(() => {
-      $b()
-        .script(3)
-        .value()
-    }).toThrow()
-  })
-})
-
-describe('complex example', () => {
-  const $b2 = $b.extend()
-  const loaderBrick = rule => conf => {
-    return $b2(conf)
-      .merge({
-        rules: [rule]
-      })
-      .value()
-  }
-
-  $b2.registerBrick({
-    loader: loaderBrick
-  })
-
-  const imageBrick = opt => conf => {
-    const defaultOpt = {
-      limit: 1000
-    }
-    return $b2(conf)
-      .loader({
-        loader: 'vue-loader',
-        options: new $b2(defaultOpt).merge(opt).value()
-      })
-      .value()
-  }
-
-  $b2.registerBrick({
-    image: imageBrick
-  })
-
-  test('should take effect ', () => {
-    expect(
-      $b2()
-        .image()
-        .value()
-    ).toEqual({
-      rules: [
-        {
-          loader: 'vue-loader',
-          options: {
-            limit: 1000
-          }
-        }
-      ]
-    })
-  })
-})
-
-describe('async brick function support', () => {
-  // sync with no return
-  const fn5 = opt => conf => {
-    conf.a = 1
-  }
-
-  // sync with return
-  const fn6 = opt => conf => {
-    conf.b = 2
-    return {
-      b: 22
-    }
-  }
-
-  // async with next but not args
-  const fn7 = opt => (conf, next) => {
-    conf.c = 3
-    next()
-  }
-
-  // async with next with data
-  const fn8 = opt => (conf, next) => {
-    conf.d = 4
-    next({
-      e: 5
-    })
-  }
-
-  const $b3 = $b
-
-  $b3.registerBrick({
-    fn5,
-    fn6,
-    fn7,
-    fn8
-  })
-
-  test('should return original conf when no bricks queue', () => {
-    const res = $b3().value()
-    expect(res).toEqual({})
-  })
-  test('sync brick no return', () => {
-    const res = $b3()
-      .fn5()
-      .value()
-    expect(res).toEqual({ a: 1 })
-  })
-  test('sync brick has return', () => {
-    expect(
-      $b3()
-        .fn6()
-        .value()
-    ).toEqual({ b: 22 })
-  })
-  test('async brick no next arg', done => {
-    $b3()
-      .fn7()
-      .value()
-      .then(res => {
-        expect(res).toEqual({
-          c: 3
+      ])
+      .then(conf => {
+        expect(conf).toEqual({
+          g: 7,
+          h: 8
         })
         done()
-      })
-  })
-  test('async brick has next arg', () => {
-    $b3()
-      .fn8()
-      .value()
-      .then(res => {
-        expect(res).toEqual({
-          e: 5
-        })
-      })
-  })
-  test('should correct when compose all', () => {
-    $b3()
-      .fn5()
-      .fn6()
-      .fn7()
-      .fn8()
-      .value()
-      .then(res => {
-        expect(res).toEqual({ e: 5 })
       })
   })
 })
--- index.js
+++ index.js
@@ -1,156 +1,50 @@
-const _ = require('lodash')
-const fse = require('fs-extra')
-const path = require('path')
-const chalk = require('chalk')
+const bricks = {
+  if: require('./lib/bricks/if'),
+  ifAsync: require('./lib/bricks/ifAsync'),
+  lay: require('./lib/bricks/lay'),
+  layAsync: require('./lib/bricks/layAsync'),
+  merge: require('./lib/bricks/merge')
+}
 
-const { isset, waterfallPromise } = require('./lib/utils')
-
-const ifBrick = require('./lib/bricks/if')
-const pipeBrick = require('./lib/bricks/pipe')
-const mergeBrick = require('./lib/bricks/merge')
-const scriptBrick = require('./lib/bricks/script')
-
-const ConfigBrick = function(seed) {
+function ConfigBrick(seed) {
   return new ConfigBrick.prototype.init(seed)
 }
 
-ConfigBrick.Name = chalk.blue('[ConfigBrick]')
+ConfigBrick.bricks = bricks
 
-ConfigBrick.prototype = {
-  init: function(conf = {}) {
-    this.conf = conf
-    this.queue = []
-    this._debug = false
-    this._async = false
-    this._file = {
-      json: null
-    }
-    return this
-  },
-  /**
-   * @param {* string } filename  the filename you will output json file
-   * @return ConfigBrick instance
-   */
-  toJson(filename = path.resolve('./config.json')) {
-    this._file.json = filename
-    return this
-  },
-  _outputJsonSync(conf) {
-    if (this._file.json) {
-      fse.outputJsonSync(this._file.json, conf, { spaces: 2 })
-    }
-  },
-  debug() {
-    this._debug = true
-    return this
-  },
-  done() {
-    return this.value()
-  },
-  value() {
-    if (this._async) {
-      return waterfallPromise(this.queue, this.conf).then(conf => {
-        this._outputJsonSync(conf)
-        return conf
-      })
-    }
-    const conf = pipeBrick(this.queue)(this.conf)
-    this._outputJsonSync(conf)
-    return conf
-  }
+ConfigBrick.prototype.init = function(seed) {
+  this.seed = seed
+  return this
 }
 
 ConfigBrick.prototype.init.prototype = ConfigBrick.prototype
-ConfigBrick.bricks = {}
-ConfigBrick.state = {}
-
-/**
- * register bricks
- * @param { object } bricks
- * @return ConfigBrick
- * @example
- *
- * const $b = require('config-brick')
- * $b.registerBrick({
- *  fn1:()=>conf=>(conf.a=1,conf)
- * })
- * // or
- * $b.use({
- *  fn1:()=>conf=>(conf.a =1,conf)
- * })
- * it will add a prototype method name 'fn1' with wrapped
- * and add this fn to ConfigBrick.bricks
- */
-ConfigBrick.registerBrick = ConfigBrick.use = function(bricks) {
-  const Ctor = this
-  if (!_.isPlainObject(bricks)) {
-    throw new Error(
-      `${
-        Ctor.Name
-      } .registerBrick options should be [object] but got ${typeof bricks}`
-    )
-  }
-
-  _.forEach(bricks, (brickFn, brickName) => {
-    addOneBrick(Ctor, brickName, brickFn)
-  })
-  return Ctor
+ConfigBrick.prototype.lay = function(...args) {
+  return bricks.lay(...args)(this.seed)
 }
-
-/**
- * original brick fn to ConfigBrick.bricks object
- * @param { string } brickName
- * @param { funciton } brickFn
- *
- */
-function addOneBrick(Ctor, brickName, brickFn) {
-  if (!_.isFunction(brickFn)) {
-    throw new Error(
-      `${
-        Ctor.Name
-      } brick [${brickName}] should be a [function] but got ${typeof brickFn}`
-    )
-  }
-
-  Ctor.bricks[brickName] = brickFn
-  Ctor.prototype[brickName] = function(...brickArgs) {
-    const fn = brickFn(...brickArgs)
-    if (fn.length > 1 && !this._async) {
-      this._async = true
-      console.log(
-        `${
-          Ctor.Name
-        } brick [${brickName}] is a async brick function, .value() now will be a Promise`
-      )
-    }
-    this.queue.push(fn)
-    return this
-  }
+ConfigBrick.prototype.layAsync = function(...args) {
+  return bricks.layAsync(...args)(this.seed)
+}
+ConfigBrick.prototype.merge = function(...args) {
+  return bricks.merge(...args)(this.seed)
 }
 
 ConfigBrick.extend = function() {
   const Super = this
-  function Sub(...args) {
-    return new Sub.prototype.init(...args)
+  function Sub() {
+    return new Sub.prototype.init(arguments)
   }
-  Sub.state = {}
-  // extends Sub.__proto__ = Super
-  Object.setPrototypeOf(Sub, Super)
   function __() {
     this.constructor = Sub
   }
   Sub.prototype = ((__.prototype = Super.prototype), new __())
-  Sub.prototype.init = function(...args) {
-    Super.prototype.init.apply(this, args)
+  Sub.prototype.init = function() {
+    Super.prototype.init.apply(this, arguments)
   }
   Sub.prototype.init.prototype = Sub.prototype
 
+  Object.setPrototypeOf(Sub, Super)
+  console.log(Object.getPrototypeOf(Sub))
   return Sub
 }
 
-module.exports = ConfigBrick.registerBrick({
-  pipe: pipeBrick,
-  if: ifBrick,
-  merge: mergeBrick,
-  script: scriptBrick
-})
+module.exports = ConfigBrick
